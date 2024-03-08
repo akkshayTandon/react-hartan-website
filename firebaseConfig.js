96b3c3fb181d4270937e1a6d010f22e3748fe6fb
akkshayat165 <76265879+akkshayTandon@users.noreply.github.com>
2024-03-09 03:23:26 +0530

    update vite config and firebase config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,6 @@
 export const firbaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_CONFIG_API_KEY ,
     authDomain: import.meta.env.VITE_FIREBASE_CONFIG_AUTH_DOMAIN,
-    // databaseURL: import.meta.env.VITE_FIREBASE_CONFIG_DATABASE_URL,
     projectId: import.meta.env.VITE_FIREBASE_CONFIG_PROJECT_ID,
     storageBucket: import.meta.env.VITE_FIREBASE_CONFIG_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.VITE_FIREBASE_CONFIG_MESSAGING_SENDER_ID,
