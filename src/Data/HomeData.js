1ddd0213a99a0be65e2b0c0a3b89c89eaba5f402
akkshayat165 <76265879+akkshayTandon@users.noreply.github.com>
2024-09-03 14:06:18 +0530

    removed duplicate data

--- src/Data/HomeData.js
+++ src/Data/HomeData.js
@@ -1,36 +1,31 @@
-export const features=[
+export const features = [
     {
-        imgSrc:"/features/easy-to-use.gif",
-        heading:"Easy to use",
-        content:"Quickly integrate Hartan components into your React app to speed up development."
+        imgSrc: "/features/easy-to-use.gif",
+        heading: "Easy to use",
+        content: "Quickly integrate Hartan components into your React app to speed up development."
     },
     {
-        imgSrc:"/features/customize.gif",
-        heading:"Customizable",
-        content:"Modify the styles to fit your brand's look and feel with ease."
+        imgSrc: "/features/customize.gif",
+        heading: "Customizable",
+        content: "Modify the styles to fit your brand's look and feel with ease."
     },
     {
-        imgSrc:"/features/fast.gif",
-        heading:"Fast Development",
-        content:"Hartan decreases the development time for your project"
+        imgSrc: "/features/fast.gif",
+        heading: "Fast Development",
+        content: "Hartan decreases the development time for your project"
     },
 ];
 
 
-export const buildWithHartan=[
+export const buildWithHartan = [
     {
-        linkSrc:"https://hartan.netlify.app",
-        imgSrc:"/screenshot/hartanSS.png",
-        heading:"Hartan",
+        linkSrc: "https://hartan.netlify.app",
+        imgSrc: "/screenshot/hartanSS.png",
+        heading: "Hartan",
     },
     {
-        linkSrc:"https://hartan.netlify.app",
-        imgSrc:"/screenshot/hartanSS.png",
-        heading:"Hartan",
-    },
-    {
-        linkSrc:"https://technoconnect.io",
-        imgSrc:"/screenshot/technoconnect-screenshot.png",
-        heading:"Technoconnect",
+        linkSrc: "https://technoconnect.io",
+        imgSrc: "/screenshot/technoconnect-screenshot.png",
+        heading: "Technoconnect",
     },
 ];
